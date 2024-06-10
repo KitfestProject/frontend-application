8343809b614366766b34dd0bb115d6cf57b5eeb0
theOnynagos <otienodennis29@gmail.com>
2024-06-10 22:47:27 +0300

    Search result area initialization

--- src/components/utils/ReusableSearchModal.jsx
+++ src/components/utils/ReusableSearchModal.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useContext } from "react";
 import { FaCircleExclamation, FaSliders } from "react-icons/fa6";
 import { SearchModal, CustomDateInput, EventTypeTab } from "@/components";
 import { SearchContext } from "@/context/SearchContext";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ReusableSearchModal = ({ show, onClose }) => {
   const { searchData, setSearchData, getEventCategories, getEventLocations } =
@@ -103,6 +104,12 @@ const ReusableSearchModal = ({ show, onClose }) => {
 
   if (!show) return null;
 
+  const filterVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 },
+  };
+
   return (
     <SearchModal onClose={onClose}>
       <div className="md:w-[600px] min-h-auto rounded-md">
@@ -112,6 +119,7 @@ const ReusableSearchModal = ({ show, onClose }) => {
             className="text-primary dark:text-slate-100"
           />
           <input
+            autoComplete="off"
             type="text"
             name="search"
             value={searchData.search}
@@ -151,140 +159,168 @@ const ReusableSearchModal = ({ show, onClose }) => {
           </div>
         )}
 
-        {showFilters && (
-          <div className="h-[550px] overflow-y-scroll">
-            <div className="h-[500px] flex flex-col justify-between">
-              <div className="p-5 pb-5">
-                <h5 className="font-semibold text-dark text-lg">
-                  Provide Filters
-                </h5>
+        <AnimatePresence>
+          {showFilters && (
+            <motion.div
+              key="filters"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={filterVariants}
+              transition={{ duration: 0.3 }}
+              className="h-[550px] overflow-y-scroll"
+            >
+              <div className="h-[500px] flex flex-col justify-between">
+                <div className="p-5 pb-5">
+                  <h5 className="font-semibold text-dark text-lg">
+                    Provide Filters
+                  </h5>
 
-                <div className="mt-3">
-                  <label
-                    htmlFor="categories"
-                    className="text-xs text-gray dark:text-slate-100 flex-1"
-                  >
-                    Select category
-                  </label>
-                  <Select
-                    options={categories}
-                    className="basic-multi-select"
-                    isMulti
-                    onChange={(selected) => {
-                      const selectedCategories = selected.map(
-                        (category) => category.value
-                      );
-                      setSearchData((prev) => ({
-                        ...prev,
-                        filters: {
-                          ...prev.filters,
-                          categories: selectedCategories,
-                        },
-                      }));
-                    }}
-                    value={getSelectedCategories()}
-                    placeholder="Select event categories..."
-                    styles={selectStyle}
-                  />
-                </div>
-
-                <div className="mt-3">
-                  <label htmlFor="eventEndDate" className="text-xs text-gray">
-                    Event Date
-                  </label>
-                  <CustomDateInput
-                    date={eventDate}
-                    handleChange={handleEventDateChange}
-                  />
-                </div>
-
-                <div className="mt-3">
-                  <label
-                    htmlFor="locations"
-                    className="text-xs text-gray dark:text-slate-100"
-                  >
-                    Select location
-                  </label>
-                  <Select
-                    options={locations}
-                    className="basic-multi-select"
-                    onChange={(selected) => {
-                      setSearchData((prev) => ({
-                        ...prev,
-                        filters: {
-                          ...prev.filters,
-                          locationId: selected.value,
-                        },
-                      }));
-                    }}
-                    value={locations.find(
-                      (location) =>
-                        location.value === searchData.filters.locationId
-                    )}
-                    isSearchable={true}
-                    placeholder="Select event locations..."
-                    styles={selectStyle}
-                  />
-                </div>
-
-                <div className="mt-3">
-                  <label
-                    htmlFor="eventType"
-                    className="text-xs text-gray dark:text-slate-100"
-                  >
-                    Event Type
-                  </label>
-                  <div className="flex gap-5 items-center w-full">
-                    <EventTypeTab
-                      type="free"
-                      selectedType={selectedType}
-                      handleChange={handleEventTypeChange}
-                    />
-                    <EventTypeTab
-                      type="paid"
-                      selectedType={selectedType}
-                      handleChange={handleEventTypeChange}
+                  <div className="mt-3">
+                    <label
+                      htmlFor="categories"
+                      className="text-xs text-gray dark:text-slate-100 flex-1"
+                    >
+                      Select category
+                    </label>
+                    <Select
+                      options={categories}
+                      className="basic-multi-select"
+                      isMulti
+                      onChange={(selected) => {
+                        const selectedCategories = selected.map(
+                          (category) => category.value
+                        );
+                        setSearchData((prev) => ({
+                          ...prev,
+                          filters: {
+                            ...prev.filters,
+                            categories: selectedCategories,
+                          },
+                        }));
+                      }}
+                      value={getSelectedCategories()}
+                      placeholder="Select categories..."
+                      styles={selectStyle}
                     />
                   </div>
+
+                  <div className="mt-3">
+                    <label htmlFor="eventEndDate" className="text-xs text-gray">
+                      Event Date
+                    </label>
+                    <CustomDateInput
+                      date={eventDate}
+                      handleChange={handleEventDateChange}
+                    />
+                  </div>
+
+                  <div className="mt-3">
+                    <label
+                      htmlFor="locations"
+                      className="text-xs text-gray dark:text-slate-100"
+                    >
+                      Select location
+                    </label>
+                    <Select
+                      options={locations}
+                      className="basic-multi-select"
+                      onChange={(selected) => {
+                        setSearchData((prev) => ({
+                          ...prev,
+                          filters: {
+                            ...prev.filters,
+                            locationId: selected.value,
+                          },
+                        }));
+                      }}
+                      value={locations.find(
+                        (location) =>
+                          location.value === searchData.filters.locationId
+                      )}
+                      isSearchable={true}
+                      placeholder="Select locations..."
+                      styles={selectStyle}
+                    />
+                  </div>
+
+                  <div className="mt-3">
+                    <label
+                      htmlFor="eventType"
+                      className="text-xs text-gray dark:text-slate-100"
+                    >
+                      Event Type
+                    </label>
+                    <div className="flex gap-5 items-center w-full">
+                      <EventTypeTab
+                        type="free"
+                        selectedType={selectedType}
+                        handleChange={handleEventTypeChange}
+                      />
+                      <EventTypeTab
+                        type="paid"
+                        selectedType={selectedType}
+                        handleChange={handleEventTypeChange}
+                      />
+                    </div>
+                  </div>
+
+                  {
+                    // Show amount input field if eventType is paid
+                    searchData.filters.eventType === "paid" ? (
+                      <motion.div
+                        key="amount"
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={filterVariants}
+                        transition={{ duration: 0.3 }}
+                        className="mt-3"
+                      >
+                        <label
+                          htmlFor="amount"
+                          className="text-xs text-gray dark:text-slate-100"
+                        >
+                          Event Amount
+                        </label>
+                        <input
+                          className="w-full text-primary bg-[#F5F5F5] dark:bg-gray p-2 rounded-md outline-none text-[15px]"
+                          type="number"
+                          name="amount"
+                          value={searchData.filters.amount}
+                          onChange={(ev) => {
+                            setSearchData((prev) => ({
+                              ...prev,
+                              filters: {
+                                ...prev.filters,
+                                amount: ev.target.value,
+                              },
+                            }));
+                          }}
+                          placeholder="Enter amount..."
+                        />
+                      </motion.div>
+                    ) : (
+                      <div className="h-[75px]"></div>
+                    )
+                  }
                 </div>
 
-                <div className="mt-3">
-                  <label
-                    htmlFor="amount"
-                    className="text-xs text-gray dark:text-slate-100"
-                  >
-                    Event Amount
-                  </label>
-                  <input
-                    className="w-full text-primary bg-[#F5F5F5] dark:bg-gray p-2 rounded-md outline-none text-[15px]"
-                    type="number"
-                    name="amount"
-                    value={searchData.filters.amount}
-                    onChange={(ev) => {
-                      setSearchData((prev) => ({
-                        ...prev,
-                        filters: {
-                          ...prev.filters,
-                          amount: ev.target.value,
-                        },
-                      }));
-                    }}
-                    placeholder="Enter event amount..."
-                  />
+                <div className="w-full flex gap-5 p-5">
+                  <button className="flex-1 bg-primary text-slate-100 py-2 rounded-md">
+                    Apply Filter
+                  </button>
+                  <button className="flex-1 bg-slate-200 text-dark py-2 rounded-md">
+                    Cancel
+                  </button>
                 </div>
               </div>
+            </motion.div>
+          )}
 
-              <div className="w-full flex gap-5 p-5">
-                <button className="flex-1 bg-primary text-slate-100 py-2 rounded-md">
-                  Apply Filter
-                </button>
-                <button className="flex-1 bg-slate-200 text-dark py-2 rounded-md">
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
+          {/* Handle Search result */}
+          {!showFilters && <div className="p-5">Search Input area</div>}
+        </AnimatePresence>
       </div>
     </SearchModal>
   );
