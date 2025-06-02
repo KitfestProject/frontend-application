0f542d1913e59f96b199093788d7c61c0e37bb3a
Joseph Kimani07 <josegee0707@gmail.com>
2025-06-02 08:54:04 +0300

    added links to top navbar

--- src/components/utils/TopNavigationMenu.jsx
+++ src/components/utils/TopNavigationMenu.jsx
@@ -1,6 +1,6 @@
 import { BiSolidPhone, BiSolidMap } from "react-icons/bi";
 import { HiOutlineMail } from "react-icons/hi";
-import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const TopNavigationMenu = () => {
@@ -10,18 +10,19 @@ const TopNavigationMenu = () => {
         {/* Company Phone Number */}
         <div className="flex gap-2 items-center">
           <BiSolidPhone className="text-xl" />
-          <p>(+254) 794 785768</p>
+          <p>
+            <a href="tel:+254794785768">(+254) 794 785768</a>
+          </p>
         </div>
 
         {/* Separator */}
         <span className="text-white ">&#8226;</span>
 
-        {/* Company Email */}
+        {/* Company Email - Icon only */}
         <div className="flex gap-2 items-center">
-          <HiOutlineMail className="text-xl" />
-          <p>
-            <a href="mailto:info@example.com">theatreke@kitfest.co.ke</a>
-          </p>
+          <a href="mailto:theatreke@kitfest.co.ke">
+            <HiOutlineMail className="text-xl" />
+          </a>
         </div>
 
         {/* Separator */}
@@ -36,12 +37,44 @@ const TopNavigationMenu = () => {
         {/* Separator */}
         <span className="text-white ">&#8226;</span>
 
-        {/* Social Media Icons */}
+        {/* Social Media Icons with links */}
         <div className="flex gap-4">
-          <FaFacebook className="text-xl" />
-          <FaXTwitter className="text-xl" />
-          <FaInstagram className="text-xl" />
-          <FaLinkedin className="text-xl" />
+          <a
+            href="https://web.facebook.com/kitfestke"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className="text-xl hover:text-blue-400 transition-colors" />
+          </a>
+          <a
+            href="https://www.instagram.com/kitfest_ke/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-xl hover:text-pink-600 transition-colors" />
+          </a>
+          <a
+            href="https://www.youtube.com/@kitfest_ke"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube className="text-xl hover:text-red-600 transition-colors" />
+          </a>
+          <a
+            href="https://x.com/kitfest_ke"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter className="text-xl hover:text-black transition-colors" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/company/kitfest-trust/posts/?feedView=all"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-xl hover:text-blue-600 transition-colors" />
+          </a>
         </div>
       </div>
     </div>
